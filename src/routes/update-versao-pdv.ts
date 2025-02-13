06e605c39d66bb126d8cad3f1c29c5fc05a3dd9d
tcolonhezi <thiago.colonhezi@gmail.com>
2025-02-13 15:11:00 -0300

    Implementado para receber array de pdvs e atualizar/inserir conforme necessário

--- src/routes/update-versao-pdv.ts
+++ src/routes/update-versao-pdv.ts
@@ -6,57 +6,70 @@ import { z } from "zod";
 
 export async function updateVersaoPdv(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
-    "/pdvs/",
+    "/pdvs/versaopdvs",
     {
       schema: {
-        body: z.object({
-          Numero: z.number(),
-          Versao: z.string(),
-          Revisao: z.string(),
-          Datacomp: z.string(),
-          Versaoso: z.string(),
-        }),
-        params: z.object({
-          cnpj: z.string(),
-        }),
+        body: z.array(
+          z.object({
+            Numero: z
+              .string()
+              .refine(
+                (val) => !isNaN(Number(val)) && Number.isInteger(Number(val)),
+                {
+                  message: "Número deve ser inteiro valido.",
+                }
+              )
+              .transform((val) => parseInt(val)),
+            Cnpj: z.string(),
+            Versao: z.string(),
+            Revisao: z.string(),
+            Datacomp: z.string(),
+            Versaoso: z.string(),
+          })
+        ),
       },
     },
     async (request, reply) => {
       console.log(request.body);
-      const { cnpj } = request.params;
-      const { Numero, Versao, Revisao, Datacomp, Versaoso } = request.body;
+      const pdvs = request.body;
+      const updatedPdvs = await Promise.all(
+        pdvs.map(
+          async ({ Numero, Versao, Revisao, Datacomp, Versaoso, Cnpj }) => {
+            const cliente = await prisma.cliente.findUnique({
+              where: {
+                cliente_cnpj_cpf: Cnpj,
+              },
+            });
 
-      const cliente = await prisma.cliente.findUnique({
-        where: {
-          cliente_cnpj_cpf: cnpj,
-        },
-      });
-
-      if (!cliente) {
-        reply.status(404).send({ message: "Cliente não encontrado" });
-      } else {
-        const pdv = await prisma.versaoPDV.upsert({
-          where: {
-            cliente_id: cliente.cliente_id,
-            pdv_numero: Numero,
-          },
-          update: {
-            versao: Versao,
-            revisao: Revisao,
-            data_compilacao: Datacomp,
-            sistema_op: Versaoso,
-          },
-          create: {
-            cliente_id: cliente.cliente_id,
-            pdv_numero: Numero,
-            versao: Versao,
-            revisao: Revisao,
-            data_compilacao: Datacomp,
-            sistema_op: Versaoso,
-          },
-        });
-        return pdv;
-      }
+            if (!cliente) {
+              reply.status(404).send({ message: "Cliente não encontrado" });
+              return null;
+            } else {
+              const pdv = await prisma.versaoPDV.upsert({
+                where: {
+                  pdv_numero: Numero,
+                },
+                update: {
+                  versao: Versao,
+                  revisao: Revisao,
+                  data_compilacao: Datacomp,
+                  sistema_op: Versaoso,
+                },
+                create: {
+                  pdv_numero: Numero,
+                  versao: Versao,
+                  revisao: Revisao,
+                  data_compilacao: Datacomp,
+                  sistema_op: Versaoso,
+                  cliente_id: cliente.cliente_id,
+                },
+              });
+              return pdv;
+            }
+          }
+        )
+      );
+      reply.send(updatedPdvs);
     }
   );
 }
