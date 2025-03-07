cdf60e05a2bece580530e36b62fc70b1d9f311c5
tcolonhezi <thiago.colonhezi@gmail.com>
2025-03-07 11:10:41 -0300

    Corrigido a verificação da versão na rota status pdv do relatorio.

--- src/routes/get-clientes-pdvs-status.ts
+++ src/routes/get-clientes-pdvs-status.ts
@@ -53,14 +53,14 @@ const PaginatedResponseSchema = z.object({
 
 // Lista de versões conforme fornecido
 const versoes = [
-  { data: "OK-17/05/2025", revisao: 31161 },
-  { data: "OK-14/06/2024", revisao: 31160 },
-  { data: "OK-09/08/2024", revisao: 31159 },
-  { data: "OK-04/10/2024", revisao: 31158 },
+  { data: "17/05/2025", revisao: 31161 },
+  { data: "14/06/2024", revisao: 31160 },
+  { data: "09/08/2024", revisao: 31159 },
+  { data: "04/10/2024", revisao: 31158 },
 ];
 
 function verificarVersao(versao: string, revisao: string) {
-  const versaoEncontrada = versoes.find((v) => v.data === versao);
+  const versaoEncontrada = versoes.find((v) => v.data === versao.split("-")[1]);
 
   if (versaoEncontrada) {
     return Number(revisao) >= versaoEncontrada.revisao
